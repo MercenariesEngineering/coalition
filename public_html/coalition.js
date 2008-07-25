9f8a19ba3515561f5c58f95a3201a519c2d0f2ee
corvazier <corvazier@02f45169-c152-0410-8fda-f97b7897b862>
2008-07-25 00:16:01 +0000

    ADDED : better web interface ADDED : remove one or all jobs ADDED : worker list

--- public_html/coalition.js
+++ public_html/coalition.js
@@ -1,16 +1,73 @@
+var xmlrpc
+var service
 $(document).ready(function()
 {
-	var xmlrpc = imprt("xmlrpc");
-	var service = new xmlrpc.ServerProxy ("/xmlrpc", ["getjobs"]);
+	xmlrpc = imprt("xmlrpc");
+	service = new xmlrpc.ServerProxy ("/xmlrpc", ["getjobs", "clearjobs", "clearjob", "getworkers", "clearworkers"]);
+	renderJobs ();
+});
+
+function clearJobs ()
+{
+	service.clearjobs ();
+	renderJobs ();
+}
+
+function clearJob (jobId)
+{
+	service.clearjob (jobId);
+	renderJobs ();
+}
+
+function clearWorkers ()
+{
+	service.clearworkers ();
+	renderWorkers ();
+}
+
+function renderJobs ()
+{
+	$("#main").empty ();
+
 	var jobs = service.getjobs ();
 	var table = "<table id='jobs'>";
-	table += "<tr id='jobtitle'><th>ID</th><th>Title</th><th>State</th><th>Worker</th><th>Try</th><th>StartTime</th><th>PingTime</th><th>Command</th></tr>\n";
+
+	function renderButtons ()
+	{
+		$("#main").append("<input type='button' name='myButton' value='Clear All Jobs' onclick='clearJobs()'>\n");
+	}
+	renderButtons ();
+	table += "<tr id='jobtitle'><th>ID</th><th>Title</th><th>State</th><th>Worker</th><th>Try</th><th>Command</th></tr>\n";
 	for (i=0; i < jobs.length; i++)
 	{
 		var job = jobs[i];
-		table += "<tr id='job'><td>"+job.ID+"</td><td>"+job.Title+"</td><td>"+job.State+"</td><td>"+job.Worker+"</td><td>"+job.Try+"</td><td>"+job.StartTime+"</td><td>"+job.PingTime+"</td><td>"+job.Command+"</td></tr>\n";
+		table += "<tr id='job'><td>"+job.ID+"</td><td>"+job.Title+"</td><td>"+job.State+"</td><td>"+job.Worker+"</td><td>"+job.Try+"</td><td>"+job.Command+"</td><td><a href='javascript:clearJob("+job.ID+")'>Remove</a></td></tr>\n";
 	}
 	table += "</table>";
 	$("#main").append(table);
-});
+	renderButtons ();
+}
+
+function renderWorkers ()
+{
+	$("#main").empty ();
+
+	var workers = service.getworkers ();
+	var table = "<table id='workers'>";
+
+	function renderButtons ()
+	{
+		$("#main").append("<input type='button' name='myButton' value='Clear All Workers' onclick='clearWorkers()'>\n");
+	}
+	renderButtons ();
+	table += "<tr id='workertitle'><th>Name</th><th>State</th><th>LastJob</th><th>Finished</th><th>Error</th></tr>\n";
+	for (i=0; i < workers.length; i++)
+	{
+		var worker = workers[i];
+		table += "<tr id='worker'><td>"+worker.Name+"</td><td>"+worker.State+"</td><td>"+worker.LastJob+"</td><td>"+worker.Finished+"</td><td>"+worker.Error+"</td></tr>\n";
+	}
+	table += "</table>";
+	$("#main").append(table);
+	renderButtons ();
+}
 
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 
 from twisted.web import xmlrpc, server, static
 from twisted.internet import defer
-import pickle, time, os
+import pickle, time, os, getopt, sys
 
 # This module is standard in Python 2.2, otherwise get it from
 #   http://www.pythonware.com/products/xmlrpc/
@@ -13,8 +13,46 @@ try:
 except OSError:
 	pass
 
-global TimeOut
+global TimeOut, port, verbose
 TimeOut = 10
+port = 8080
+verbose = False
+
+def usage():
+	print ("Usage: server [OPTIONS]")
+	print ("Start a Coalition server.\n")
+	print ("Options:")
+	print ("  -p, --port=PORT\tPort used by the servre (default: "+str(port)+")")
+	print ("  -h, --help\t\tShow this help")
+	print ("  -v, --verbose\t\tIncrease verbosity")
+	print ("\nExample : worker -s 30 -v http://localhost:8080")
+
+# Parse the options
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hp:v", ["help", "port=", "verbose"])
+	if len(args) != 0:
+		usage()
+		sys.exit(2)
+except getopt.GetoptError, err:
+	# print help information and exit:
+	print str(err) # will print something like "option -a not recognized"
+	usage()
+	sys.exit(2)
+for o, a in opts:
+	if o in ("-h", "--help"):
+		usage ()
+		sys.exit(2)
+	elif o in ("-v", "--verbose"):
+		verbose = True
+	elif o in ("-p", "--port"):
+		port = float(a)
+	else:
+		assert False, "unhandled option " + o
+
+# Log function
+def output (str):
+	if verbose:
+		print (str)
 
 class Job:
 	"""A farm job"""
@@ -29,17 +67,30 @@ class Job:
 		self.PingTime = self.StartTime		# Last worker ping time
 		self.Try = 0				# Number of try
 
+class Worker:
+	"""A farm worker"""
+
+	def __init__ (self, name):
+		self.Name = name			# Worker name
+		self.State = "WAITING"			# Job state, can be WAITING, WORKING, FINISHED or TIMEOUT
+		self.PingTime = time.time()		# Last worker ping time
+		self.Finished = 0			# Number of finished
+		self.Error = 0				# Number of fault
+		self.LastJob = -1			# Last job done
+
 # State of the master
 class CState:
 
 	Counter = 0
 	Jobs = []
+	Workers = []
 
 	# Read the state
 	def read (self, fo):
 		version = pickle.load(fo)
 		self.Counter = pickle.load(fo)
 		self.Jobs = pickle.load(fo)
+		self.Workers = pickle.load(fo)
 
 	# Write the state
 	def write (self, fo):
@@ -47,9 +98,27 @@ class CState:
 		pickle.dump(version, fo)
 		pickle.dump(self.Counter, fo)
 		pickle.dump(self.Jobs, fo)
+		pickle.dump(self.Workers, fo)
 
 class Master(xmlrpc.XMLRPC):
 	"""    """
+
+	def getWorker (self, name):
+		found = False
+		for i in range(len(self.State.Workers)) :
+			worker = self.State.Workers[i]
+			if worker.Name == name :
+				worker.PingTime = time.time()
+				found = True
+				return worker
+		
+		# Job not found, add it
+		if not found:
+			output ("Add worker " + name)
+			worker = Worker (name)
+			worker.PingTime = time.time()
+			self.State.Workers.append (worker)
+			return worker
 
 	def update (self):
 		global TimeOut
@@ -59,87 +128,139 @@ class Master(xmlrpc.XMLRPC):
 		for job in self.State.Jobs:
 			# Check if the job is timeout
 			if job.State == "WORKING" and _time - job.PingTime > TimeOut :
-				print ("Job " + str(job.ID) + " timeout")
-				job.State = "WAITING"
+				output ("Job " + str(job.ID) + " timeout")
+				job.State = "ERROR"
+				worker = self.getWorker (job.Worker)
+				worker.State = "TIMEOUT"
+				worker.Error = worker.Error+1
 
-#		print ("Jobs:")
+#		output ("Jobs:")
 #		for job in self.State.Jobs:
-#			# Print the job
-#			print ("\t"+str(job.ID)+" "+job.Title+" \""+job.Command+"\" "+job.State+" "+job.Worker+" "+str(job.Try)+" try")
+#			# output the job
+#			output ("\t"+str(job.ID)+" "+job.Title+" \""+job.Command+"\" "+job.State+" "+job.Worker+" "+str(job.Try)+" try")
 
 	def xmlrpc_addjob(self, title, cmd):
 		"""Show the command list."""
-		print ("Add job : " + cmd)
+		output ("Add job : " + cmd)
 		self.State.Jobs.append (Job(self.State.Counter, title, cmd))
 		self.State.Counter = self.State.Counter + 1
 		self.update ()
 		return 1
 
 	def xmlrpc_getjobs(self):
-		print ("Send jobs")
-		self.update ();
+		output ("Send jobs")
+		self.update ()
 		return self.State.Jobs
 
-	def xmlrpc_log(self, hostname, jobId, log):
+	def xmlrpc_clearjobs(self):
+		output ("Clear jobs")
+		self.State.Jobs = []
+		self.update ()
+		return 1
+
+	def xmlrpc_clearjob(self, jobId):
+		output ("Clear job"+str(jobId))
+		# Look for the job
+		for i in range(len(self.State.Jobs)) :
+			job = self.State.Jobs[i]
+			if job.ID == jobId :
+				self.State.Jobs.pop (i)
+				break;
+		self.update ()
+		return 1
+
+	def xmlrpc_getworkers(self):
+		output ("Send workers")
+		self.update ()
+		return self.State.Workers
+
+	def xmlrpc_clearworkers(self):
+		output ("Clear workers")
+		self.State.Workers = []
+		self.update ()
+		return 1
+
+	def xmlrpc_heartbeat(self, hostname, jobId, log):
 		"""Add some logs."""
-		print ("Logs for " + str(jobId))
-		# Look for a job
-		if log != "" :
-			for i in range(len(self.State.Jobs)) :
-				job = self.State.Jobs[i]
-				if job.ID == jobId and job.State == "WORKING" and job.Worker == hostname:
+		output ("Heart beat for " + str(jobId))
+
+		# Ping the worker
+		worker = self.getWorker (hostname)
+
+		# Look for the job
+		for i in range(len(self.State.Jobs)) :
+			job = self.State.Jobs[i]
+			if job.ID == jobId and job.State == "WORKING" and job.Worker == hostname:
+				if log != "" :
 					job.PingTime = time.time()
 					try:
 						logFile = open ("logs/" + str(jobId) + ".log", "a")
 						logFile.write (log)
 						logFile.close ()
 					except IOError:
-						print ("Error in logs");
-					return 1
-		return 1
+						output ("Error in logs")
+				# Continue
+				return True
+		# Stop
+		worker.State = "WAITING"
+		return False
 
 	def xmlrpc_pickjob(self, hostname):
 		"""A worker ask for a job."""
-		print (hostname + " wants some job")
+		output (hostname + " wants some job")
+
+		# Ping the worker
+		worker = self.getWorker (hostname)
+
 		# Look for a job
 		for i in range(len(self.State.Jobs)) :
 			job = self.State.Jobs[i]
 			if job.State == "WAITING":
-				job.State =	"WORKING"
+				job.State = "WORKING"
 				job.Worker = hostname
 				job.Try = job.Try + 1
 				job.PingTime = time.time()
 				self.update ()
+				worker.State = "WORKING"
+				worker.LastJob = job.ID
 				return job.ID, job.Command
 		self.update ()
 		return -1,""
 
 	def xmlrpc_endjob(self, hostname, jobId, errorCode):
 		"""A worker ask for a job."""
-		print (hostname + " wants some job")
+		output ("End job " + str(jobId) + " with code " + str(errorCode))
+
+		# Ping the worker
+		worker = self.getWorker (hostname)
+		worker.State = "WAITING"
+
 		# Look for a job
 		for i in range(len(self.State.Jobs)) :
 			job = self.State.Jobs[i]
 			if job.ID == jobId:
 				if errorCode == 0:
 					job.State =	"FINISHED"
+					worker.Finished = worker.Finished + 1
 				else:
 					job.State =	"ERROR"
+					worker.Error = worker.Error + 1
+
 				self.update ()
 				return 1
 		self.update ()
-		return ""
+		return 1
 
 	# Write the DB on disk
 	def saveDb (self):
 		fo = open("master_db", "wb")
 		self.State.write (fo)
 		fo.close()
-		print ("DB saved")
+		output ("DB saved")
 
 	# Read the DB from disk
 	def readDb (self):
-		print ("Read DB")
+		output ("Read DB")
 		try:
 			fo = open("master_db", "rb")
 			try:
@@ -149,7 +270,7 @@ class Master(xmlrpc.XMLRPC):
 				print ("Error reading master_db, create a new one")
 				self.State = self.CState()
 		except IOError:
-			print ("No db found, create a new one")
+			output ("No db found, create a new one")
 			return
 
 	State = CState()
@@ -162,7 +283,7 @@ def main():
 	xmlrpc.readDb ()
 	xmlrpc.update ()
 	root.putChild('xmlrpc', xmlrpc)
-	reactor.listenTCP(8080, server.Site(root))
+	reactor.listenTCP(port, server.Site(root))
 	reactor.run()
 
 
