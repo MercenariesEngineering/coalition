fa238bd25d7c939f6a64d114c86f90a08f9fc5b0
corvazier <corvazier@02f45169-c152-0410-8fda-f97b7897b862>
2008-07-27 20:02:59 +0000

    ADDED: Job priority ADDED: Worker load ADDED: Job Duration ADDED: Directory to run the job ADDED: Add command can add several frames at once ADDED: Better web interface with a first css

--- public_html/coalition.js
+++ public_html/coalition.js
@@ -1,10 +1,14 @@
-var xmlrpc
-var service
+var xmlrpc;
+var service;
+var timer;
+var page = "jobs";
+var logId = 0;
+
 $(document).ready(function()
 {
 	xmlrpc = imprt("xmlrpc");
-	service = new xmlrpc.ServerProxy ("/xmlrpc", ["getjobs", "clearjobs", "clearjob", "getworkers", "clearworkers"]);
-	renderJobs ();
+	service = new xmlrpc.ServerProxy ("/xmlrpc", ["getjobs", "clearjobs", "clearjob", "getworkers", "clearworkers", "getlog"]);
+	timerCB ();
 });
 
 function clearJobs ()
@@ -19,10 +23,54 @@ function clearJob (jobId)
 	renderJobs ();
 }
 
+function renderLog (jobId)
+{
+	logId = jobId;
+	$("#main").empty ();
+	var _log = service.getlog (jobId);
+	$("#main").append("<pre class='logs'><h2>Logs for jod "+jobId+":</h2>"+_log+"</pre>");
+
+	page = "logs";
+}
+
 function clearWorkers ()
 {
 	service.clearworkers ();
 	renderWorkers ();
+}
+
+function formatDuration (secondes)
+{
+	var days = Math.floor (secondes / (60*60*24));
+	var hours = Math.floor ((secondes-days*60*60*24) / (60*60));
+	var minutes = Math.floor ((secondes-days*60*60*24-hours*60*60) / 60);
+	var secondes = Math.floor (secondes-days*60*60*24-hours*60*60-minutes*60);
+	if (days > 0)	
+		return days + " d " + hours + " h " + minutes + " m " + secondes + " s";
+	if (hours > 0)	
+		return hours + " h " + minutes + " m " + secondes + " s";
+	if (minutes > 0)	
+		return minutes + " m " + secondes + " s";
+	return secondes + " s";
+}
+
+// Timer callback
+function timerCB ()
+{
+	refresh ();
+
+	// Fire a new time event
+	// timer=setTimeout("timerCB ()",4000);
+}
+
+function refresh ()
+{
+	if (page == "jobs")
+		renderJobs ();
+	else if (page == "workers") 
+		renderWorkers ();
+	else if (page == "logs") 
+		renderLog (logId);
 }
 
 function renderJobs ()
@@ -30,22 +78,25 @@ function renderJobs ()
 	$("#main").empty ();
 
 	var jobs = service.getjobs ();
-	var table = "<table id='jobs'>";
+	var table = "<table id='jobs' border=0 cellspacing=1 cellpadding=0>";
 
 	function renderButtons ()
 	{
 		$("#main").append("<input type='button' name='myButton' value='Clear All Jobs' onclick='clearJobs()'>\n");
 	}
 	renderButtons ();
-	table += "<tr id='jobtitle'><th>ID</th><th>Title</th><th>State</th><th>Worker</th><th>Try</th><th>Command</th></tr>\n";
+	table += "<tr class='title'><th>ID</th><th>Title</th><th>State</th><th>Priority</th><th>Worker</th><th>Duration</th><th>Try</th><th>Command</th><th>Directory</th><th>Tools</th></tr>\n";
 	for (i=0; i < jobs.length; i++)
 	{
 		var job = jobs[i];
-		table += "<tr id='job'><td>"+job.ID+"</td><td>"+job.Title+"</td><td>"+job.State+"</td><td>"+job.Worker+"</td><td>"+job.Try+"</td><td>"+job.Command+"</td><td><a href='javascript:clearJob("+job.ID+")'>Remove</a></td></tr>\n";
+		table += "<tr class='entry"+(i%2)+"'><td>"+job.ID+"</td><td>"+job.Title+"</td><td class='"+job.State+"'>"+job.State+"</td><td>"+job.Priority+"</td><td>"+job.Worker+"</td><td>"+formatDuration (job.Duration)+"</td><td>"+job.Try+"/"+job.Retry+"</td><td>"+job.Command+"</td><td>"+job.Dir+
+		"</td><td><a href='javascript:renderLog("+job.ID+")'>Log</a> <a href='javascript:clearJob("+job.ID+")'>Remove</a></td></tr>\n";
 	}
 	table += "</table>";
 	$("#main").append(table);
 	renderButtons ();
+
+	page = "jobs";
 }
 
 function renderWorkers ()
@@ -60,14 +111,16 @@ function renderWorkers ()
 		$("#main").append("<input type='button' name='myButton' value='Clear All Workers' onclick='clearWorkers()'>\n");
 	}
 	renderButtons ();
-	table += "<tr id='workertitle'><th>Name</th><th>State</th><th>LastJob</th><th>Finished</th><th>Error</th></tr>\n";
+	table += "<tr class='title'><th>Name</th><th>State</th><th>Load</th><th>LastJob</th><th>Finished</th><th>Error</th></tr>\n";
 	for (i=0; i < workers.length; i++)
 	{
 		var worker = workers[i];
-		table += "<tr id='worker'><td>"+worker.Name+"</td><td>"+worker.State+"</td><td>"+worker.LastJob+"</td><td>"+worker.Finished+"</td><td>"+worker.Error+"</td></tr>\n";
+		table += "<tr class='entry"+(i%2)+"'><td>"+worker.Name+"</td><td class='"+worker.State+"'>"+worker.State+"</td><td>"+worker.Load+"</td><td>"+worker.LastJob+"</td><td>"+worker.Finished+"</td><td>"+worker.Error+"</td></tr>\n";
 	}
 	table += "</table>";
 	$("#main").append(table);
 	renderButtons ();
+
+	page = "workers";
 }
 
