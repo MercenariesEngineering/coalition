e8eb16ee5a847899b42b5bfeaa2dfa94ba623c53
mp.etienne <mp.etienne@02f45169-c152-0410-8fda-f97b7897b862>
2009-01-18 18:45:46 +0000

    addjob becomes control.py. control.py allows to add job and to list jobs on the coalition server

--- control.py
+++ control.py
@@ -1,6 +1,6 @@
 import xmlrpclib, sys, getopt
 
-global cmd, serverUrl, dir, title
+global cmd, serverUrl, dir, title, action
 dir = "."
 title = "New job"
 startIndex = 1
@@ -10,28 +10,31 @@ affinity = ""
 priority = 1000
 
 def usage():
-	print ("Usage: addjob [OPTIONS] SERVER_URL COMMAND")
-	print ("Add a job COMMAND to a Coalition server located at SERVER_URL.\n")
+	print ("Usage: control.py [OPTIONS] SERVER_URL ACTION [COMMAND]")
+	print ("Control the Coalition server located at SERVER_URL.\n")
+	print("Actions:")
+	print("  add: add a job, use option -c for command")
+	print("  list: list the jobs on the server")
 	print ("Options:")
 	print ("  -h, --help\t\tShow this help")
+	print ("  -c, --command=COMMAND\t\tIf action is add, add command to server")
 	print ("  -d, --directory=DIR\tWorking directory (default: "+dir+")")
 	print ("  -t, --title=TITLE\tSet the job title (default: "+title+")")
-	print ("  -s, --start=INDEX\tSet the index of the first job (default: "+str(startIndex)+")")
-	print ("  -e, --end=INDEX\tSet the index of the last job (default: "+str(endIndex)+")")
 	print ("  -p, --priority=PRIORITY\tPriority of the job (default: "+str(priority)+")")
 	print ("  -r, --retry=RETRY\tNumber of retry this jobs can do (default: "+str(retry)+")")
 	print ("  -a, --affinity=AFFINITY\tAffinity words to workers, separated by a comma (default: \"\"")
 	print ("  -v, --verbose\t\tIncrease verbosity")
-	print ("\nExample : addjob -t \"Job%04d\" -s 1 -e 10 -a \"hello,world\" http://localhost:8080 \"echo Hello world!\"")
+
+	print ("\nExample : control -t \"Job\" -a \"Linux\" -c \"echo Hello world!\" http://localhost:8080 add")
 
 # Parse the options
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "a:d:e:hr:s:t:v", ["affinity=", "directory=", "end=", "help", "retry=", "start=", "title=", "verbose="])
+	opts, args = getopt.getopt(sys.argv[1:], "a:d:e:hr:s:t:v:c:", ["affinity=", "directory=", "end=", "help", "retry=", "start=", "title=", "verbose=", "command="])
 	if len(args) != 2 :
 		usage()
 		sys.exit(2)
 	serverUrl = args[0]
-	cmd = args[1]
+	action = args[1]
 except getopt.GetoptError, err:
 	# print help information and exit:
 	print str(err) # will print something like "option -a not recognized"
@@ -57,6 +60,8 @@ for o, a in opts:
 		affinity = a
 	elif o in ("-t", "--title"):
 		title = a
+	elif o in ("-c", "--command"):
+		cmd=a
 	else:
 		assert False, "unhandled option " + o
 
@@ -66,12 +71,11 @@ def output (str):
 		print (str)
 
 server = xmlrpclib.ServerProxy(serverUrl + "/xmlrpc")
-for i in range(startIndex, endIndex+1):
-#	output ("Add job : "+title % i+cmd % i+dir % i)
-	def format (s, i):
-		try:
-			return s % i
-		except TypeError:
-			return s
-	server.addjobwithaffinity (format (title, i), format (cmd, i), format (dir, i), priority, retry, affinity)
 
+if action=="add":
+	server.addjobwithaffinity (title, cmd, dir, priority, retry, affinity)
+	print("job added")
+elif action=="list":
+	jobs=server.getjobs()
+	for i in range(len(jobs)):
+		print(str(jobs[i]["ID"])+" "+str(jobs[i]["Title"])+" "+str(jobs[i]["State"])+" "+str(jobs[i]["Priority"])+" "+str(jobs[i]["Affinity"])+" "+str(jobs[i]["Worker"])+" "+str(jobs[i]["Duration"])+" "+str(jobs[i]["Try"])+" "+str(jobs[i]["Command"])+" "+str(jobs[i]["Dir"]))
