640d004ca44ab27bc95e5e8b1f0885fbc45eba8f
corvazier <corvazier@02f45169-c152-0410-8fda-f97b7897b862>
2009-07-29 22:52:42 +0000

    FIXED : win32 installer install now for everybody FIXED : win32 installer create a shared writable directory in ProgramData ADDED : control.exe in the win32 distribution

--- setup_py2exe.py
+++ setup_py2exe.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 import py2exe
-setup(service=['server'], console=['worker.py'], windows=['win32_network_drives.py'], options = {"py2exe": { "dll_excludes": ["MSWSOCK.dll","POWRPROF.dll"]}})
+setup(service=['server'], console=['worker.py','control.py'], options = {"py2exe": { "dll_excludes": ["MSWSOCK.dll","POWRPROF.dll"]}})
