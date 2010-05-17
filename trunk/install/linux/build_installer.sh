4ec511b50484e699f5582ba4cd4587b55f4735f6
Mp.Etienne <Mp.Etienne@02f45169-c152-0410-8fda-f97b7897b862>
2010-05-17 03:01:06 +0000

    FIXED : build_installer.sh doesn't include the .svn files in the archive

--- trunk/install/linux/build_installer.sh
+++ trunk/install/linux/build_installer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ../..
-tar -cvzf install/linux/coalition.tar.gz coalition.ini control.py job.py LICENCE public_html/*   server.py worker.py
+tar -cvzf install/linux/coalition.tar.gz coalition.ini control.py job.py LICENCE public_html/*   server.py worker.py --exclude=".svn"
