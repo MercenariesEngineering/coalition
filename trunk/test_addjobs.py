c67dfd44fd5851cff0d08ed83aa8a3c58e61c32e
corvazier@gmail.com <corvazier@gmail.com@02f45169-c152-0410-8fda-f97b7897b862>
2010-11-09 13:24:52 +0000

    Version 3 Lot of improvements and fixes. New JS GUI design. Progression of jobs. Monitor core usage and CPU.

---
+++ trunk/test_addjobs.py
@@ -0,0 +1,12 @@
+import os
+
+nbP = 1	# nb parents
+nbC = 1000	# nb children
+
+for p in range(0,nbP):
+	pipe = os.popen ('python control.py -c "" -t "Parent job' + str(p) + '" http://127.0.0.1:19211 add')
+	parent = int(pipe.read ())
+
+	for c in range(0,nbC):
+		pipe = os.popen ('python control.py -c "python job.py" -t "Job ' + str(c) + '" -P ' + str(parent) + ' http://127.0.0.1:19211 add')
+		child = int(pipe.read ())
