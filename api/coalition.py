033feeb2e5cf72ddb8c10ccdf9c326d24393dcde
Cyril Corvazier <corvazier@mercs-eng.com>
2016-01-18 11:43:48 +0100

    [Doc] first API documentation

--- api/coalition.py
+++ api/coalition.py
@@ -4,6 +4,10 @@ class CoalitionError(Exception):
 	pass
 
 class Job(object):
+	''' A job object returned by the :class:`Connection`. Don't create such objects yourself.
+
+	'''
+
 	def __init__ (self, d, conn):
 		assert (conn)
 		self.Conn = False
@@ -23,6 +27,12 @@ class Job(object):
 		super(Job, self).__setattr__(attr, value)
 
 class Connection:
+	''' A connection to the coalition server.
+
+	:param str host: the coalition server hostname
+	:param int port: the coalition server port (19211 by default)
+	'''
+
 	def __init__(self, host, port):
 		self.IntoWith = False
 		self._Conn = httplib.HTTPConnection (host, port)
@@ -40,10 +50,19 @@ class Connection:
 			raise CoalitionError (res.read())
 
 	def getJob (self, id):
+		''' Returns a :class:`Job` object.
+
+		:param id: the id of the :class:`Job` to return
+		'''
 		res = self._send ('getJob', locals().copy ())
 		return Job (json.loads(res), self)
 
 	def getJobChildren (self, id):
+		''' Returns a :class:`Job` children objects.
+
+		:param id: the job.ID of the parent :class:`Job`
+		:rtype: A list of :class:`Job` objects
+		'''
 		res = self._send ('getJobChildren', locals().copy ())
 		res = json.loads(res)
 		children = []
@@ -52,6 +71,11 @@ class Connection:
 		return children
 
 	def getJobDependencies (self, id):
+		''' Returns the :class:`Job` objects on which a job has a dependency.
+
+		:param id: the job.ID of the :class:`Job` with dependencies
+		:rtype: The list of :class:`Job` objects on which the job depends
+		'''
 		res = self._send ('getJobDependencies', locals().copy ())
 		res = json.loads(res)
 		children = []
@@ -60,10 +84,51 @@ class Connection:
 		return children
 
 	def setJobDependencies (self, id, ids):
+		''' Set the :class:`Job` objects on which a job has a dependency.
+
+		:param id: the id of the job with dependencies
+		:rtype: The list of job.ID (int) on which the job depends
+		'''
 		res = self._send ('setJobDependencies', locals().copy ())
 		return res
 
 	def newJob  (self, parent=0, title="", command = "", dir = "", environment = "", state="WAITING", priority = 1000, retry = 10, timeout = 0, affinity = "", user = "", dependencies = []):
+		''' Create a job.
+
+		:param parent: the parent job.ID
+		:type parent: int
+		
+		:param title: the job title
+		:type title: str
+		
+		:param command: the job command
+		:type command: str
+		
+		:param dir: the job directory. This is the current directory when the job is run.
+		:type dir: str
+		
+		:param environment: the job environment variables
+		:type environment: str
+		
+		:param state: the job initial state. It must be "WAITING" or "PAUSED". If the state is "WAITING", the job will start as soon as possible. If the state is "PAUSED", the job won't start until it is started or reset.
+		:type state: str
+
+		:param priority: the job priority. For a given job hierarchy level, the job with the biggest priority is taken first.
+		:type priority: int
+		
+		:param retry: the job retry count. If the job fails, the server will retry to run it this number of time.
+		:type retry: int
+		
+		:param affinity: the job affinity string. Affinities are coma separated keywords. To run a job, the worker affinities must match all the job affinities.
+		:type affinity: str
+		
+		:param user: the job user name.
+		:type user: str
+		
+		:param [int] dependencies: the jobs id on which the new job has dependencies. The job will run when the dependency jobs have been completed without error.
+
+		:rtype: The list of job id (int) on which the job depends
+		'''
 		res = self._send ('newJob', locals().copy ())
 		return int(res)
 
---
+++ doc/source/index.rst
@@ -0,0 +1,23 @@
+.. Coalition documentation master file, created by
+   sphinx-quickstart on Mon Jan 18 10:12:25 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Coalition's documentation!
+=====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+.. automodule:: coalition
+   :members:
+   
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
